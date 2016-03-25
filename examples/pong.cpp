747e1fecbbdaf1dae28f439e2b27f3603b35bc30
Adam Stevenson <adamstevenson121@gmail.com>
2016-03-25 09:54:11 -0400

    Create pong.cpp
    simulate a ball bouncing around in a friction-less box

---
+++ examples/pong.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "glovebox.h"
+
+int main(int argc, char ** argv) {
+
+	using namespace std;
+	using namespace glovebox;
+
+	struct vec2 {
+		float x;
+		float y;
+		vec2(float x, float y) :x(x), y(y) {}
+	};
+
+	Param<vec2> pos, speed;
+
+	pos = Param<vec2>(vec2(0, 0), ALWAYS, [&]() {
+		return vec2(pos().x + speed().x, pos().y + speed().y);
+	});
+
+	speed = Param<vec2>(vec2(-1.f, 0.75f), ALWAYS, [&]() {
+		vec2 r(speed().x, speed().y);
+		if (pos().x < -4 || pos().x > 4)
+			r.x *= -1;
+		if (pos().y < -4 || pos().y > 4)
+			r.y *= -1;
+		return r;
+	});
+
+	for (int i = 0; i < 10; i++) {
+		pos.update();
+		speed.update();
+		cout << pos().x << ", " << pos().y << endl;
+	}
+	system("PAUSE");
+}
